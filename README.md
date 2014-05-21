3dafd929bffd66425410a8652cd42fa9bdac5cf1
Duane Sibilly <duane@sibilly.com>
2014-05-21 16:57:55 -0400

    First implementation

---
+++ README.md
@@ -0,0 +1,26 @@
+# dhammatalks 0.0.1
+
+**dhammatalks** is a simple JavaScript website scraper that transforms the Evening Talks Archive at [dhammatalks.org](http://dhammatalks.org/mp3_index.html) into a podcast RSS XML feed.
+
+## Usage
+```
+$ npm start
+
+> dhammatalks@0.0.1 start /path/to/dhammatalks
+> NODE_ENV=production node app.js
+
+Saved podcast RSS to /path/to/dhammatalks/evening.xml
+```
+
+## Config
+Config parameters are in env.json, and available to app.js via the config object.
+
+## Dependencies
+**dhammatalks** uses the following npm modules:
+
+- [cheerio](https://github.com/cheeriojs/cheerio) for jQuery-style DOM implementation
+- [jade](https://github.com/visionmedia/jade) for templating
+- [request](https://github.com/mikeal/request) for simple HTTP requests
+
+## License
+**dhammatalks** is distributed under the MIT License. See LICENSE for details.
---
+++ common.js
@@ -0,0 +1,43 @@
+/**
+common.js
+A set of common useful code for use across a project. Commonly used to bootstrap
+applications with settings for development and production environments.
+*/
+(function () {
+    'use strict';
+    
+    // Import the config vars in env.json
+    var env = require('./env.json');
+    
+    // A recursive object merge function
+    Object.prototype.merge = function(other_obj) {
+        var p;
+        
+        for (p in other_obj) {
+            if (other_obj.hasOwnProperty(p)) {
+                try {
+                    // Property in this object is set; update its value.
+                    if (other_obj[p] instanceof Object) {
+                        this[p].merge(other_obj[p]);
+                    } else {
+                        this[p] = other_obj[p];
+                    }
+                } catch (e) {
+                    // Property in this object NOT set; create and set value.
+                    this[p] = other_obj[p];
+                }
+            }
+        }
+    };
+    
+    exports.config = function () {
+        var node_env = process.env.NODE_ENV || 'development';
+        
+        if (env.all) {
+            // Merge global config values into config object!
+            env[node_env].merge(env.all);
+        }
+        
+        return env[node_env];
+    };
+}());
